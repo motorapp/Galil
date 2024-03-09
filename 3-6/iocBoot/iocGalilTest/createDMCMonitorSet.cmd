94461c8d80f347b66780ce6550cadf8836bfa601
ics <ics@localhost>
2024-03-09 21:54:09 +1100

    Change top directory

--- 3-6/iocBoot/iocGalilTest/createDMCMonitorSet.cmd
+++
@@ -1,9 +0,0 @@
-##################################################################################################
-# Create an autosave monitor set for a DMC instance
-##################################################################################################
-# Save motor positions every 5 seconds
-create_monitor_set("GalilTest_$(PORT)Positions.req", 5,"P=$(P)")
-# Save motor settings every 30 seconds
-create_monitor_set("GalilTest_$(PORT)Settings.req", 30,"IOC=$(IOCPREFIX),P=$(P)")
-# Save kinematics every 30 seconds
-create_monitor_set("GalilTest_$(PORT)Kinematics.req", 30,"P=$(P)")
