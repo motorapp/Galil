df16f998937bc89d172ba642752b3e5286bb9dc5
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2026-07-06 18:59:19 +0100

    Print timeout

--- GalilSup/src/GalilController.cpp
+++ GalilSup/src/GalilController.cpp
@@ -5775,6 +5775,9 @@ asynStatus GalilController::readDataRecord(char *input, unsigned bytesize)
          if (j > 0) {
              std::cerr << "readDataRecord(): discarding message " << mesg << " length " << j << std::endl;
              }
+         if (status == asynTimeout) {
+             std::cerr << "readDataRecord(): timeout after " << (async_records_ ? pasynUserAsyncGalil_->timeout : pasynUserSyncGalil_->timeout) << std::endl;
+         }
          return asynError;//Stop if any asyn error
      }
      readsize -= nread;
@@ -5864,9 +5867,9 @@ asynStatus GalilController::sync_writeReadController(bool testQuery, bool logCom
   size_t len;
   static const char* debug_file_name = macEnvExpand("$(GALIL_DEBUG_FILE=)");
   static FILE* debug_file = ( (debug_file_name != NULL && strlen(debug_file_name) > 0) ? fopen(debug_file_name, "at") : NULL);
-//  if (!this->havelock()) {
-//      std::cerr << "sync_writeReadController problem 1" << std::endl;
-//  }
+  if (!this->havelock()) {
+      std::cerr << "sync_writeReadController problem 1" << std::endl;
+  }
   if (++call_count != 1) {
       std::cerr << "sync_writeReadController problem 2" << std::endl;
   }
